1a587f5b8efe28a33326c81afd44fcc208ee0ba0
Lamzydev <hallamublessin1994@gmail.com>
2026-07-14 22:56:19 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <section id="center">
+
+   <div id="root">
+<h1>Welcome to Week 1 Of FlyRank AI!</h1>
+ <p>In this week, we will be focusing on the basics of AI and how it can be applied to various industries. We will also be exploring the different types of AI and their applications.</p>
+
+     </div>
+
+       <div className="hero">
+          <ul>
+            <li>
+              <a href="https://github.com/vitejs/vite" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#github-icon"></use>
+                </svg>
+                GitHub
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      
+    </>
+  )
+}
+
+export default App
